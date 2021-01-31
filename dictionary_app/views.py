6eaaf1f1f6352b7a1761e9299ff1ec8323fd51f4
Oliver Otchere <oliverotchere4@gmail.com>
2021-01-31 18:22:37 +0000

    views for homepage

--- dictionary_app/views.py
+++ dictionary_app/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render
+import requests
+import os
+
+from .helpers import searchWord
 
 def home(request):
-    return render(request,'dictionary_app/index.html')
+
+    context={}
+
+    if request.method=='POST':
+
+        user_word=request.POST.get('user_word')
+
+
+        result=searchWord(user_word)
+
+        print(result)
+       
+        try:
+            context={
+                'search_word':result['word'],
+                'results':result['results'],
+                'pronunciations':result['pronunciation']
+            }
+
+        except KeyError:
+            context={
+                'error':result['message'],
+                'failure': True
+            }
+
+       
+
+    return render(request,'dictionary_app/index.html',context=context)
+    
